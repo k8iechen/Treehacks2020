300c15f9e19c721cf9a4551be09dfb12e25b5101
Aryan Arora <aarora@stratcomm.ucla.edu>
2020-02-15 17:18:08 -0800

    File Upload

---
+++ frontend-ios/frontend-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  frontend-ios
+//
+//  Created by Aryan Arora on 2/15/20.
+//  Copyright © 2020 Aryan Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ frontend-ios/frontend-iosTests/frontend_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  frontend_iosTests.swift
+//  frontend-iosTests
+//
+//  Created by Aryan Arora on 2/15/20.
+//  Copyright © 2020 Aryan Inc. All rights reserved.
+//
+
+import XCTest
+@testable import frontend_ios
+
+class frontend_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
